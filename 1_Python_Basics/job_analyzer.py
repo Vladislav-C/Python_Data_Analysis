d0bbf21e2d809bf52b2ed37df71417d8fc2568f3
Vladislav-C <vladislavprimem@gmail.com>
2026-01-19 21:50:29 +0100

    first commit

---
+++ 1_Python_Basics/job_analyzer.py
@@ -0,0 +1,26 @@
+def calculate_salary (base_salary, bonus_rate=.1):
+    """
+    Calculate the total salary based on the base salary and bonus rate.
+
+    Args:
+        base_salary (float): The base salary.
+        bonus_rate (float): The bonus rate. Default is .1.
+
+    Returns:
+        float: The total salary
+    """
+    return base_salary * (1 + bonus_rate)
+
+def calculate_bonus (total_salary, base_salary):
+    """
+    Calculate the bonus rate based on the total salary.
+
+    Args:
+        total_ salary (float): The total salary.
+        base_salary (float): The base salary.
+
+    Returns:
+        float: The bonus
+    """
+    return (total_salary - base_salary) / base_salary
+
